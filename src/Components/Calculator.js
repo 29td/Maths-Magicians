1d0cf461561cdc204a613255cc5703002e1c5115
29td <nonhlanhlamndebele9@gmail.com>
2022-08-10 19:12:14 +0200

    calculator

---
+++ src/Components/Calculator.js
@@ -0,0 +1,84 @@
+/* eslint-disable react/jsx-no-bind */
+/* eslint-disable  react/prefer-stateless-function */
+import React, { useEffect, useState } from 'react';
+import Row from './RowCalcul';
+
+export default function Calculator() {
+  const [dataObj, setDataObj] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
+
+  function updateDataObj(event) {
+    const value = event.target.textContent;
+    setDataObj((prevDataObj) => calculate(prevDataObj, value));
+  }
+
+  function updateScreen() {
+    const screen = document.querySelector('.result-screen');
+    if (dataObj.next && !dataObj.total) {
+      screen.value = dataObj.next;
+    } else if (!dataObj.next && dataObj.total) {
+      screen.value = dataObj.operation
+        ? `${dataObj.total}  ${dataObj.operation}`
+        : dataObj.total;
+    } else if (!dataObj.total && !dataObj.next) {
+      screen.value = '';
+    } else {
+      screen.value = `
+        ${dataObj.total} ${
+  dataObj.operation
+    ? `${dataObj.operation} ${dataObj.next}`
+    : dataObj.next
+}`;
+    }
+  }
+
+  useEffect(() => {
+    updateScreen();
+  }, [dataObj]);
+
+  return (
+    <div className="calculator">
+      <h1 className="heading">Calc( )</h1>
+      <input type="text" className="result-screen" value={0} readOnly />
+      <div className="buttons">
+        <Row
+          updateDataObj={updateDataObj}
+          first="AC"
+          second="+/-"
+          third="%"
+          fourth="/"
+        />
+        <Row
+          updateDataObj={updateDataObj}
+          first="7"
+          second="8"
+          third="9"
+          fourth="*"
+        />
+        <Row
+          updateDataObj={updateDataObj}
+          first="4"
+          second="5"
+          third="6"
+          fourth="-"
+        />
+        <Row
+          updateDataObj={updateDataObj}
+          first="1"
+          second="2"
+          third="3"
+          fourth="+"
+        />
+        <Row
+          updateDataObj={updateDataObj}
+          first="0"
+          second="."
+          third="="
+        />
+      </div>
+    </div>
+  );
+}
---
+++ src/Components/RowCalcul.js
@@ -0,0 +1,35 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+
+// eslint-disable-next-line react/prefer-stateless-function
+
+export class Row extends React.PureComponent {
+    render() {
+      const {
+        first, second, third, fourth, updateDataObj,
+      } = this.props;
+  
+      return (
+        <div className="row">
+          <button onClick={updateDataObj} type="button">{first}</button>
+          <button onClick={updateDataObj} type="button">{second}</button>
+          <button onClick={updateDataObj} type="button">{third}</button>
+          { fourth ? <button onClick={updateDataObj} type="button">{fourth}</button> : null}
+        </div>
+      );
+    }
+  }
+  
+  Row.defaultProps = {
+    fourth: '',
+  };
+  
+  Row.propTypes = {
+    first: PropTypes.string.isRequired,
+    second: PropTypes.string.isRequired,
+    third: PropTypes.string.isRequired,
+    fourth: PropTypes.string,
+    updateDataObj: PropTypes.func.isRequired,
+  };
+  
+  export default Row;
