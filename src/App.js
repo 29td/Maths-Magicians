664f200698c86b01418932109140edc678e177c8
29td <nonhlanhlamndebele9@gmail.com>
2022-08-17 11:01:31 +0200

    full website

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import './App.css';
 import React from 'react';
-import Calculator from './Components/Calculator';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/home';
+import CalculatorContainer from './pages/Container';
+import Quote from './pages/quotes';
 
-class App extends React.Component {
+class App extends React.PureComponent {
   render() {
-    return <Calculator />;
+    return (
+      <Router>
+        <div className="App">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/Calculator" element={<CalculatorContainer />} />
+            <Route path="/pages/quotes" element={<Quote />} />
+          </Routes>
+        </div>
+      </Router>
+    );
   }
 }
+
 export default App;
---
+++ src/pages/Container.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Calculator from '../Components/Calculator';
+import Navbar from '../Components/Navbar';
+
+const CalculatorContainer = () => (
+  <div className="calcContainer">
+    <Navbar />
+    <Calculator />
+  </div>
+);
+
+export default CalculatorContainer;
