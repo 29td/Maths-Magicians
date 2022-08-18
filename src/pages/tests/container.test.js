3b211adc3f1dd6ae0bdcd4f1b3580672fff48522
29td <nonhlanhlamndebele9@gmail.com>
2022-08-18 15:24:35 +0200

    added testing for the page

---
+++ src/pages/tests/container.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import CalculatorContainer from '../Container';
+
+test('should render the Quote page', () => {
+  const calcContainer = render(<Router><CalculatorContainer /></Router>);
+  expect(calcContainer).toMatchSnapshot();
+});
